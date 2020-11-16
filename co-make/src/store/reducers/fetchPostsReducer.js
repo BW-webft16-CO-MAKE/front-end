a66f46dc95bc283e33fe1d7338b7fc003967ad4e
MorganWilliamson <mwilliamson7695@gmail.com>
2020-11-16 15:36:48 -0600

    Working on reducers.

--- co-make/src/store/reducers/fetchPostsReducer.js
+++ co-make/src/store/reducers/fetchPostsReducer.js
@@ -23,7 +23,79 @@ const initialState = {
 
 export function reducer (state = initialState, action) {
     switch(action.type) {
-        
+        // CREATE/POST
+        case ADD_POST_START:
+            return {
+                ...state,
+            };
+        case ADD_POST_SUCCESS:
+            // ?
+            return {
+                ...state, 
+                posts: action.payload,
+                updated: true,
+            };
+        case ADD_POST_FAILURE:
+            return {
+                ...state,
+                updated: false,
+                error: action.payload,
+            }
+
+        // READ/GET
+        case FETCH_POST_START:
+            return {
+                ...state,
+                isLoading: true,
+            };
+        case FETCH_POST_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                posts: action.payload
+            };
+        case FETCH_POST_FAILURE:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload,
+            };
+
+        // UPDATE/PUT
+        case EDIT_POST_START:
+            return {
+                ...state,
+            };
+        case EDIT_POST_SUCCESS:
+            return {
+                ...state,
+                updated: true,
+                // ?
+            };
+        case EDIT_POST_FAILURE:
+            return {
+                ...state,
+                updated: false,
+                error: action.payload,
+            };
+
+        // DELETE
+        case DELETE_POST_START:
+            return {
+                ...state,
+            };
+        case DELETE_POST_SUCCESS:
+            return {
+                ...state,
+                updated: true,
+                // ?
+            };
+        case DELETE_POST_FAILURE:
+            return {
+                ...state,
+                updated: false,
+                error: action.payload,
+            };
         
         default: 
             return state;
