bb20ef5cb935f02b97431b87012e9e4a69e2bfda
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2020-11-17 08:25:19 -0700

    test main page content

--- co-make/src/App.js
+++ co-make/src/App.js
@@ -1,37 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom'
-
 import './App.css';
 import MainPage from './components/MainPage';
 import SignUp from './components/SignUp';
 // import { SignIn } from './components/SignIn'
-
-
 function App() {
   const initialSignUpValues = {
     className: ""
   }
   const [formValues, setFormValues] = useState(initialSignUpValues)
-
   return (
     <div>
       <Switch>
         <Route exact path='/'>
           <MainPage />
         </Route>
-
         {/* <Route path='/signin'>
           <SignIn />
         </Route> */}
-
         <Route path='/signup'>
           <SignUp />
         </Route>
-
       </Switch>
-
     </div>
   );
 };
-
 export default App
--- co-make/src/components/signup.js
+++ co-make/src/components/signup.js
@@ -1,42 +1,85 @@
 import React from 'react';
-import SignIn from './SignIn';
-import { Route, Link } from 'react-router-dom';
+// import SignIn from './SignIn';
+// import { Link } from 'react-router-dom';
 // import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
-const SignUp = (props) => {
-    const { change, submit } = props;
+// const SignUp = () => {
+//     // const { change, submit } = props;
 
-    const onChange = evt => {
-        const { formInput } = evt.target
-        change(formInput)
-    }
-    const onSubmit = evt => {
-        evt.preventDefault();
-        submit()
-    }
+//     // const onChange = evt => {
+//     //     const { formInput } = evt.target
+//     //     change(formInput)
+//     // }
+//     // const onSubmit = evt => {
+//     //     evt.preventDefault();
+//     //     submit()
+//     // }
+//     return (
+//         // <form onSubmit={onSubmit}>
+//         <form>
+//             <h3> Sign Up</h3>
+//             <div className="formContent">
+//                 <label>First name</label>
+//                 <input type="text" className="formInput" onChange={onChange} placeholder="First name" />
+//             </div>
+//             <div className="formContent">
+//                 <label>Last name</label>
+//                 <input type="text" className="formInput" onChange={onChange} placeholder="Last name" />
+//             </div>
+//             <div className="formContent">
+//                 <label>Email</label>
+//                 <input type="text" className="formInput" onChange={onChange} placeholder="Email" />
+//             </div>
+//             <div className="formContent">
+//                 <label>Password</label>
+//                 <input type="text" className="formInput" onChange={onChange} placeholder="Password" />
+//             </div>
+//             <div className="submit">
+//                 <button type="submit" onClick="/">Sign Up</button>
+//                 <p className="forgot-password">Already Registered
+//                     <li><Link to='/signin'>sign in?</Link></li>
+//                 </p>
+//             </div>
+//         </form>
+//     )
+// }
+// export default SignUp
+
+const SignUp = () => {
+    // const { change, submit } = props;
+
+    // const onChange = evt => {
+    //     const { formInput } = evt.target
+    //     change(formInput)
+    // }
+    // const onSubmit = evt => {
+    //     evt.preventDefault();
+    //     submit()
+    // }
     return (
-        <form onSubmit={onSubmit}>
+        // <form onSubmit={onSubmit}>
+        <form>
             <h3> Sign Up</h3>
             <div className="formContent">
                 <label>First name</label>
-                <input type="text" className="formInput" onChange={onChange} placeholder="First name" />
+                <input type="text" className="formInput" placeholder="First name" />
             </div>
             <div className="formContent">
                 <label>Last name</label>
-                <input type="text" className="formInput" onChange={onChange} placeholder="Last name" />
+                <input type="text" className="formInput" placeholder="Last name" />
             </div>
             <div className="formContent">
                 <label>Email</label>
-                <input type="text" className="formInput" onChange={onChange} placeholder="Email" />
+                <input type="text" className="formInput" placeholder="Email" />
             </div>
             <div className="formContent">
                 <label>Password</label>
-                <input type="text" className="formInput" onChange={onChange} placeholder="Password" />
+                <input type="text" className="formInput" placeholder="Password" />
             </div>
             <div className="submit">
-                <button type="submit" onClick={submit}>Sign Up</button>
+                <button type="submit" onClick="/">Sign Up</button>
                 <p className="forgot-password">Already Registered
-                    <li><Link to='/signin'>sign in?</Link></li>
+                    {/* <li><Link to='/signin'>sign in?</Link></li> */}
                 </p>
             </div>
         </form>
