6a8b5fefd46a4aa294f8ba3cf1f1018303f33c8e
Dionne Stratton <mylife33@hotmail.com>
2020-11-17 16:46:49 -0600

    requesting all posts for page

--- co-make/src/components/AllPosts.js
+++ co-make/src/components/AllPosts.js
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
+
 const AllPosts = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getAllPosts = () => {
+    axiosWithAuth()
+      .get("/posts")
+      .then((res) => {
+        console.log("All Posts", res);
+        setPosts(res.data.data());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getAllPosts();
+    console.log();
+  }, []);
+
   return (
-    <div>
-      <h1>Make me a get request!</h1>
+    <div className="postspage">
+      <section className="section">
+        <h3>Posts</h3>
+        <br></br>
+        <div className="posts">
+          {posts.map((post) => (
+            <div className="item">
+              <p>{post.post_name}</p>
+              <p>{post.post_description}</p>
+              <p>{post.post_location}</p>
+            </div>
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
+
 export default AllPosts;
