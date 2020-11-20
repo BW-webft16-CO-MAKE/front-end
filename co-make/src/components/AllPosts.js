8b22bd3d1a5aa41bc32f59984caa674aff39f9c3
Dionne Stratton <mylife33@hotmail.com>
2020-11-19 18:28:12 -0600

    update delete not working

--- co-make/src/components/AllPosts.js
+++ co-make/src/components/AllPosts.js
@@ -1,15 +1,56 @@
 import React, { useEffect, useState } from "react";
 import axiosWithAuth from "../utils/axiosWithAuth";
+import { useHistory, useParams } from "react-router-dom";
 
-const AllPosts = () => {
-  const [posts, setPosts] = useState([]);
+// const initialValues = {
+//   name: "",
+//   description: "",
+//   location: "",
+// };
+
+const AllPosts = ({ posts, updatePost }) => {
+  const [allPosts, setAllPosts] = useState([]);
+  const { id } = useParams();
+  const history = useHistory();
+  //   const [editing, setEditing] = useState(false);
+  //   const [postToEdit, setPostToEdit] = useState(initialValues);
+
+  //   const editPost = (post) => {
+  //     setEditing(true);
+  //     setPostToEdit(post);
+  //   };
+
+  //   const saveEdit = (e) => {
+  //     e.preventDefault();
+  //     axiosWithAuth()
+  //       .put(`api/posts/:${postToEdit.id}`, postToEdit)
+  //       .then((res) => {
+  //         setEditing(false);
+  //         updatePost(
+  //           posts.map((post) => {
+  //             return post.id === postToEdit.id ? res.data : post;
+  //           })
+  //         );
+  //       })
+  //       .catch((err) => console.log(err));
+  //     console.log(postToEdit);
+  //   };
+
+  const deletePost = (post) => {
+    axiosWithAuth()
+      .delete(`api/posts/:${id}`)
+      .then((res) => {
+        updatePost(posts.filter((post) => post.id !== res.data));
+      })
+      .catch((err) => console.log(err));
+  };
 
   const getAllPosts = () => {
     axiosWithAuth()
       .get("api/posts")
       .then((res) => {
         console.log("All Posts", res);
-        setPosts(res.data);
+        setAllPosts(res.data);
       })
       .catch((err) => {
         console.log(err);
@@ -17,6 +58,7 @@ const AllPosts = () => {
   };
 
   useEffect(() => {
+    deletePost();
     getAllPosts();
     console.log();
   }, []);
@@ -27,11 +69,22 @@ const AllPosts = () => {
         <h3 className="Posts">Posts</h3>
         <br></br>
         <div className="posts">
-          {posts.map((post) => (
+          {allPosts.map((post) => (
             <div className="item">
               <p>Name: {post.post_name}</p>
               <p>Descrription: {post.post_description}</p>
               <p>Location: {post.post_location}</p>
+              <button
+                className="update-button"
+                onClick={() => {
+                  history.push(`/update-post/${id}`);
+                }}
+              >
+                Update
+              </button>
+              <button className="update-button" onClick={deletePost}>
+                Delete
+              </button>
             </div>
           ))}
         </div>
